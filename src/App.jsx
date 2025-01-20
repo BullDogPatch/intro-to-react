ebe3df1a18dcf384605f4a791efecafd5cc67619
Craig Clayton <craig__clayton@outlook.com>
2025-01-20 13:24:31 +0000

    boom

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [isVisible, setIsvisible] = useState(true);
+
+  const handleVisiblity = () => setIsvisible((prev) => !prev);
+
+  return (
+    <>
+      <header>
+        <h1>Finally we got to React</h1>
+      </header>
+      <div>
+        <button onClick={handleVisiblity}>
+          {isVisible ? 'hide content' : 'show content'}
+        </button>
+        {isVisible && (
+          <p>This is the part in the course I have been looking forward to.</p>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
